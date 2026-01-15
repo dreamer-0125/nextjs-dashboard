de275c2bccc09828174d6d9128a683e7e52e3052
dreamer-0125 <catchdream125@gmail.com>
2026-01-15 01:41:35 -0800

    remove streaming errors

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,6 +1,5 @@
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { fetchCardData } from '@/app/lib/data';
-import {Card} from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
@@ -12,12 +11,6 @@ export default async function Page() {
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
-            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Card title="Collected" value={totalPaidInvoices} type="collected" />
-                <Card title="Pending" value={totalPendingInvoices} type="pending" />
-                <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-                <Card title="Total Customers" value={numberOfCustomers} type="customers" />
-            </div>
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
