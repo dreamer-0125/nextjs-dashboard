75b2c4e02eb638719e44c0c964f67b7474459729
dreamer-0125 <catchdream125@gmail.com>
2026-01-20 20:10:46 -0800

    add error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,6 +64,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
   } catch (error) {
     // If a database error occurs, return a more specific error.
+    console.error(error);
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
@@ -102,6 +103,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error(error);
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
 
@@ -110,6 +112,8 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+  
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
